fd0522786814e8d739290869eedb945e8fef8b65
Nodrex5 <100078617+Nodrex5@users.noreply.github.com>
2024-01-04 14:39:58 +0300

    Update falcon911.py

--- falcon911.py
+++ falcon911.py
@@ -55,7 +55,7 @@ def infoTool():
 os.system("clear")
 # Function to print gradient logo
 def print_gradient_logo():
-    logo = pyfiglet.figlet_format('Falcon 911', font='starwars')
+    logo = pyfiglet.figlet_format('Falcon 911', font='speed')
     gradient_colors = [
         '\033[91m',
 
