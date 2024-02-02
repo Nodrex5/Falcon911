5f92f2457fffc52104bd3bd820dfbb35a4834a91
Mustafa22 <100078617+Nodrex5@users.noreply.github.com>
2024-02-02 03:30:23 +0300

    Update f.py

--- f.py
+++ f.py
@@ -13,45 +13,43 @@ __series__ = 'Attack.'
 
 # -----------
 os.system('clear')
+logo()
 def infoTool():
-	spinner = Halo()
-	spinner.start()
-	spinner.succeed("Author  : %s" % __author__)
-	spinner.succeed("Version : %s."%__version__)
-	
+        spinner = Halo()
+        spinner.start()
+        spinner.succeed("Author  : %s" % __author__)
+        spinner.succeed("Version : %s."%__version__)
+
 
 infoTool()
 print(f"{Fore.LIGHTBLACK_EX}-- Falcon 911 -- {Fore.RESET}")
 
 # --------
 methodList = [
-	'UDP',
-	'TCP',
-	'HTTP',
+        'UDP',
+        'TCP',
+        'HTTP',
 ]
 
-print(f'''
-{Fore.GREEN}―――――――――――――――――――――――――――――――――――――
-1. {Fore.RED}UDP.
-{Fore.GREEN}2. {Fore.RED}TCP (soon...)
-{Fore.GREEN}3. {Fore.RED}HTTP.
-{Fore.GREEN}―――――――――――――――――――――――――――――――――――――{Fore.RESET}
-''')
-print(f'{Fore.CYAN}[ ~ ] Choose a DDOS attack method.')
+print(f'{Fore.GREEN}—'*40)
+print(f'''{Fore.GREEN}1. {Fore.LIGHTRED_EX}UDP.
+{Fore.GREEN}2. {Fore.LIGHTRED_EX}TCP (soon...)
+{Fore.GREEN}3. {Fore.LIGHTRED_EX}HTTP.''')
+print(f'{Fore.GREEN}—'*40)
+print(f'{Fore.GREEN}[ ~ ]{Fore.RESET} Choose a DDOS attack method.')
 try:
-	methodInput = (int(input(f'{Fore.GREEN}[ ~ ]{Fore.RED} method ~#{Fore.GREEN}')))
-	if methodInput==1:
-		os.system('clear')
-		subprocess.run(["python","fu.py"])
-	elif methodInput==2:
-		print('soon ...')
-	elif methodInput==3:
-		os.system('clear')
-		subprocess.run(["python","fh.py"])
-	else:
-		print(f'{Fore.RED}[ ! ]{Fore.CYAN} Please choose one of the numbers in the list.')
+        methodInput = (int(input(f'{Fore.GREEN}[ ~ ]{Fore.LIGHTRED_EX} method ~#{Fore.GREEN}')))
+        if methodInput==1:
+                os.system('clear')
+                subprocess.run(["python","fu.py"])
+        elif methodInput==2:
+                print('soon ...')
+        elif methodInput==3:
+                os.system('clear')
+                subprocess.run(["python","fh.py"])
+        else:
+                print(f'{Fore.RED}[ ! ]{Fore.CYAN} Please choose one of the numbers in the list.')
 except ValueError as e:
-	print(e)
-
-
-
+        print(e)
+        
+        
